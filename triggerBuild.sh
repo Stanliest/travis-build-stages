a0f50c66e2498488e0b33bc2a5bef89c632edf3c
Stanley Liu <stanley.liu@ibm.com>
2021-03-04 13:53:38 -0500

    trying out replacing build config

--- triggerBuild.sh
+++ triggerBuild.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-body='{"request": {"branch":"develop"}}'
+body='{"request":{"branch":"develop","merge_mode":"replace","config":{"jobs":{"include":[{"stage":"Test triggered by API only","script":["echo \"test should only be triggered by API\""],"env":[{"TestAPI":""}]}]},"stages":[{"name":"Test triggered by API only","if":"branch = develop"}]}}}'
 
 curl -s -X POST \
   -H 'Accept: application/json' \
-  -H "Authorization: token $TRAVIS_API_TOKEN" \
+  -H "Authorization: token RPrY-clgva9rlbasGWQsXw \
   -H 'Content-Type: application/json' \
   -H 'Travis-API-Version: 3' \
   -d "$body" \
