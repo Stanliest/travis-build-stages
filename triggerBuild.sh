3331a361924d07929aa5e697298c1a0455bf1b13
Stanley Liu <stanley.liu@ibm.com>
2021-03-04 14:11:13 -0500

    update

---
+++ triggerBuild.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+body='{"request": {"branch":"develop"}}'
+
+curl -s -X POST \
+  -H 'Accept: application/json' \
+  -H "Authorization: token $TRAVIS_API_TOKEN \
+  -H 'Content-Type: application/json' \
+  -H 'Travis-API-Version: 3' \
+  -d '$body' \
+  https://api.travis-ci.com/repo/Stanliest%2Ftravis-build-stages/requests
