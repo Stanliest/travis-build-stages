68413c0423464d0be4e665f56087b7868668c997
Stanley Liu <stanley.liu@ibm.com>
2021-03-04 11:12:23 -0500

    post request to build works on local

--- triggerBuild.sh
+++ triggerBuild.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-body='{
-  "request": {
-  "branch":"develop"
-  }
-}'
+body='{"request": {"branch":"develop"}}'
 
-curl -X POST \
+curl -s -X POST \
   -H 'Accept: application/json' \
   -H "Authorization: token $TRAVIS_API_TOKEN" \
   -H 'Content-Type: application/json' \
   -H 'Travis-API-Version: 3' \
-  -d '{"request": {"branch":"develop"}}' \
-  https://api.travis-ci.com/Stanliest/travis-build-stages/requests
+  -d "$body" \
+  https://api.travis-ci.com/repo/Stanliest%2Ftravis-build-stages/requests
