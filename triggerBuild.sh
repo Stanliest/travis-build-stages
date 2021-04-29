b1d407213df59dec59f753029b2899230af98a33
Stanley Liu <stanley.liu@ibm.com>
2021-04-29 13:51:57 -0400

    test fail job

---
+++ triggerBuild.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+BRANCH=$1
+
+body='{
+  "request": {
+    "branch":"'${BRANCH}'",
+    "message": "This build is triggered by Travis API",
+    "merge_mode": "replace",
+    "config": {
+      "jobs": {
+        "include": [
+          {
+            "stage": "Test triggered by API only",
+            "script": [
+              "echo \"test should only be triggered by API\""
+            ],
+            "env": "TestAPI"
+          },
+          {
+            "stage": "Test triggered by API only 2",
+            "script": [
+              "echo \"test should only be triggered by API\""
+            ],
+            "env": "TestAPI2"
+          }
+        ]
+      },
+      "stages": [
+        {
+          "name": "Test triggered by API only",
+          "if": "branch = develop"
+        }
+      ]
+    }
+  }
+}'
+
+curl -s -X POST \
+  -H 'Accept: application/json' \
+  -H "Authorization: token RPrY-clgva9rlbasGWQsXw" \
+  -H 'Content-Type: application/json' \
+  -H 'Travis-API-Version: 3' \
+  -d "$body" \
+  https://api.travis-ci.com/repo/Stanliest%2Ftravis-build-stages/requests
