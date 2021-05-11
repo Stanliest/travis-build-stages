f33df7e3651c0c5e9de051ce25cdd79dec189334
Stanley Liu <stanley.liu@ibm.com>
2021-05-11 15:11:48 -0400

    change POST into PUT

--- merge-PR.sh
+++ merge-PR.sh
@@ -4,9 +4,7 @@ echo "PR number: ${TRAVIS_PULL_REQUEST}"
 echo "Repo name: ${TRAVIS_REPO_SLUG#*/}"
 echo "Repo name: ${REPO_NAME}"
 
-curl -s -X POST \
-  -H 'Accept: application/json' \
-  -H "Authorization: token $token_token_token" \
-  -H 'Content-Type: application/json' \
-  -H 'Travis-API-Version: 3' \
-  https://api.travis-ci.com/repo/Stanliest%2Ftravis-build-stages/pulls/${TRAVIS_PULL_REQUEST}/merge
+curl -s -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token ${token_token_token}" \
+  https://api.github.com/repos/Stanliest/travis-build-stages/pulls/${TRAVIS_PULL_REQUEST}/merge
