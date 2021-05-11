4e38fb32a48a17e41984403e71d455883f9d2d8e
Stanley Liu <stanley.liu@ibm.com>
2021-05-11 13:08:49 -0400

    syntax for token

--- triggerSanity.sh
+++ triggerSanity.sh
@@ -11,10 +11,10 @@ body='{
       ],
       "after_success": [
         "curl -s -X POST \
-          -H 'Accept: application/json' \
-          -H 'Authorization: token $token_token_token' \
-          -H 'Content-Type: application/json' \
-          -H 'Travis-API-Version: 3' \
+          -H "Accept: application/json" \
+          -H "Authorization: token '${token_token_token}'" \
+          -H "Content-Type: application/json" \
+          -H "Travis-API-Version: 3" \
           https://api.travis-ci.com/repo/Stanliest%2Ftravis-build-stages/pulls/'${TRAVIS_PULL_REQUEST}'/merge"
       ]
     }
@@ -22,10 +22,10 @@ body='{
 }'
 
 curl -s -X POST \
-  -H 'Accept: application/json' \
+  -H "Accept: application/json" \
   -H "Authorization: token $token_token_token" \
-  -H 'Content-Type: application/json' \
-  -H 'Travis-API-Version: 3' \
+  -H "Content-Type: application/json" \
+  -H "Travis-API-Version: 3" \
   -d "$body" \
   https://api.travis-ci.com/repo/Stanliest%2Ftravis-build-stages/requests
  
