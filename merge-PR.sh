1274cf58daad65631fa5869e0e0c4adfddcf809c
Stanley Liu <stanley.liu@ibm.com>
2021-05-11 15:26:25 -0400

    test without title smh

--- merge-PR.sh
+++ merge-PR.sh
@@ -4,7 +4,7 @@ echo "PR number: ${TRAVIS_PULL_REQUEST}"
 echo "Repo name: ${TRAVIS_REPO_SLUG#*/}"
 echo "Repo name: ${REPO_NAME}"
 
-curl -s -X PUT \
+curl -X PUT \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token ${token_token_token}" \
   https://api.github.com/repos/Stanliest/travis-build-stages/pulls/${TRAVIS_PULL_REQUEST}/merge
